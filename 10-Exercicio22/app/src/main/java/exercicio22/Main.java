729ca6ec797277ca4b1a90792702bf64df572d36
kennedy <dkennyvs@gmail.com>
2022-07-28 11:42:08 -0300

    Exercicio 22

---
+++ 10-Exercicio22/app/src/main/java/exercicio22/Main.java
@@ -0,0 +1,28 @@
+package exercicio22;
+
+/**
+ * Faça um algoritmo que receba o preço de cursto e o preço de venda de 
+ * 40 produtos. Mostre como resultado se houve lucro, prejuízo ou empate para 
+ * cada produto . Informe o valor de custo de cada produto, o valor de venda
+ * de cada produto, a média de preço de custo e do preço de venda. 
+ */
+
+import java.io.PrintStream;
+import java.util.Scanner;
+ 
+ public class Main {
+  
+  public static void main(String[] args) {
+    
+    PrintStream out = System.out;
+    Scanner sc = new Scanner(System.in);
+
+    String nomeProd;
+    float PrecoDecusto;
+    float PrecoDeVenda;
+
+    for (int i = 0; i < 40; i++) {
+      
+    }
+  }  
+}
